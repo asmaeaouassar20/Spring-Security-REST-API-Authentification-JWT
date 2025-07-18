cbc7784754a856eb57177a30054e03d9df02118b
asmae <asmaeaouassar333@example.com>
2025-07-18 11:12:57 +0100

    commenter la classe CustomUserDetailsService

--- src/main/java/com/algostyle/authentification/CustomUserDetailsService.java
+++ src/main/java/com/algostyle/authentification/CustomUserDetailsService.java
@@ -10,12 +10,22 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-@Service
+
+/**
+ * Cette classe est une implémentation personnalisée du service UserDetailsService de Spring Security,
+ * utilisé pour charger les détails d'un utilisateur pendant le processus d'authentification
+ */
+
+@Service    // Marque que cette classe comme un service Spring qui sera géré par le conteneur IoC
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
+    @Autowired  //Pour injecter automatiquement une instance de UserRepository
     private UserRepository userRepository;
 
+
+    /**
+     * Implémentation de l'interface UserDetailsService
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> user = userRepository.findByUsername(username);
@@ -24,10 +34,13 @@ public class CustomUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("Utilisateur non trouvé : " + username);
         }
 
+        /**
+         * On récupère l'utilisateur trouvé, et on créé un builder pour construire un objet UserDetails
+         */
         User foundUser = user.get();
         UserBuilder builder = org.springframework.security.core.userdetails.User.withUsername(foundUser.getUsername());
         builder.password(foundUser.getPassword());
-        builder.authorities("USER"); // Vous pouvez ajouter des rôles ici
+        builder.authorities("USER"); // On peut ajouter des rôles ici
 
         return builder.build();
     }
